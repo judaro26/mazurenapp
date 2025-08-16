5ae7f984302a66f4f5c8eb8151efb22a8386d7d4
judaro26 <131112287+judaro26@users.noreply.github.com>
2025-08-15 20:03:47 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // scan all JS/JSX/TS/TSX files
+    "./public/index.html"          // scan index.html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
