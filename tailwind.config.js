757784ae184b0750c3036ac469ba34577b5044d1
judaro26 <131112287+judaro26@users.noreply.github.com>
2025-08-15 20:11:57 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // scan all JS/JSX/TS/TSX files
-    "./public/index.html"          // scan index.html
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
