7d40d82b0cd01e6a7c4fbda70fac2dc2b31c98e6
judaro26 <131112287+judaro26@users.noreply.github.com>
2025-08-23 21:27:15 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -249,7 +249,7 @@ function loadFirebaseConfig() {
     apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-    storageBucket: 'portalmalaga.appspot.com',
+    storageBucket: 'portalmalaga2025.appspot.com',
     messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
   };
